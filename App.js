e274e44802b1c296f06833f5b0a2d854af2b6869
mythri <mythrinfyxpert@gmail.com>
2023-10-19 18:15:16 +0530

    food app

---
+++ App.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function App() {
+  const menuItems = [
+    {
+      id: '1',
+      name: 'Spaghetti Carbonara',
+      cuisine: 'Italian',
+      fontWeight: 'bold',
+      price: 600,
+      image: require('./assets/food/pic1.jpg'),
+      description: 'Delicious pasta dish with creamy sauce and bacon.',
+    },
+    {
+      id: '2',
+      name: 'Sushi Rolls',
+      cuisine: 'Japanese',
+      price: 15.49,
+      image: require('./assets/food/pic2.jpg'),
+      description: 'Fresh and tasty sushi rolls with various fillings.',
+    },
+    {
+      id: '3',
+      name: 'Chicken Tikka Masala',
+      cuisine: 'Indian',
+      price: 200,
+      image: require('./assets/food/pic3.jpg'),
+      description: 'Spicy chicken curry served with rice and naan.',
+    },
+    {
+      id: '4',
+      name: 'Burger and Fries',
+      cuisine: 'American',
+      price: 300,
+      image: require('./assets/food/pic4.jpg'),
+      description: 'Classic burger and crispy fries.',
+    },
+    {
+      id: '5',
+      name: 'Pad Thai',
+      cuisine: 'Thai',
+      price: 650,
+      image: require('./assets/food/pic5.jpg'),
+      description: 'Stir-fried noodles with shrimp and vegetables.',
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#FF6B6B', '#FF6BCF']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.header}
+      >
+        <Text style={styles.headerText}>Food App</Text>
+      </LinearGradient>
+
+      <FlatList
+        data={menuItems}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.menuItem}>
+            <Image source={item.image} style={styles.itemImage} />
+            <View style={styles.itemDetails}>
+              <Text style={styles.itemName}>{item.name}</Text>
+              <Text style={styles.itemCuisine}>{item.cuisine}</Text>
+              <Text style={styles.itemPrice}>${item.price.toFixed(2)}</Text>
+              <Text style={styles.itemDescription}>{item.description}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  header: {
+    padding: 15,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'Sans-Serif',
+    textShadowColor: 'rgba(0, 0, 0, 0.75',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  itemImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  itemDetails: {
+    flex: 1,
+  },
+  itemName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  itemCuisine: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  itemPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  itemDescription: {
+    fontSize: 14,
+    color: 'black',
+  },
+});
